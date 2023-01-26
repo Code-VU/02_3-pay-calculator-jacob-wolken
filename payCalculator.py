437b06170bdd2c8120716627082507d47435b15e
Jacob Wolken <Jacob.Wolken@vu.com>
2023-01-26 11:33:08 -0600

    changed variables to integers and multiplied

--- payCalculator.py
+++ payCalculator.py
@@ -2,8 +2,10 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
-
+    hrs = int(input("Enter Hours: "))
+    rate = int(input("Enter Rate: "))
+   
+        print(hrs * rate)
     # end assignment
 
 
